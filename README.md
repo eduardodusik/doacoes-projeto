cffd1dac4ced2f4f2a20351ecec72d6b408608de
Eduardo Dusik <eduardogdusik@gmail.com>
2022-06-13 20:48:56 -0300

    chore: add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Functions necessarias para o funcionamento do sistema
+
+- API para cadastrar evento  // cadastro eventos
+- API para obter 1 evento   //  id -> recupero o evento por id
+- API para listar eventos  //   -> listo eventos
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,31 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+  response.send({name: "Eduardo"});
+});
+
+
+
+export const registrarEvento = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  response.send({name: "Eduardo"});
+});
+
+
+export const obterEvento = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  response.send({name: "Eduardo"});
+});
+
+
+export const listarEventos = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  response.send({name: "Eduardo"});
 });
