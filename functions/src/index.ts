ac7c684cea3a6a126650e6f3123adffcb10977e1
nickcoimbra <nicolascoimbra2@gmail.com>
2022-06-19 00:41:32 -0300

    Create CRUD 'api/v1/eventos'

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,33 +1,87 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp()
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+const db = admin.firestore()
+const app = express()
+const main = express()
 
-  response.send({name: "Eduardo"});
+main.use('/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+//initialize the database and the collection 
+const eventoCollection = 'eventos';
+
+//define google cloud function name
+export const api = functions.https.onRequest(main);
+
+interface Evento {
+  name: String,
+  typeEvent: String,
+  location: String,
+  initDate: Date,
+  endDate: Date,
+}
+
+// Cria um novo evento
+app.post('/eventos', async (req, res) => {
+  try {
+      const evento: Evento = {
+        name: req.body['name'],
+        typeEvent: req.body['typeEvent'],
+        location: req.body['location'],
+        initDate: req.body['initDate'],
+        endDate: req.body['endDate'],
+      }
+
+      const newDoc = await db.collection(eventoCollection).add(evento);
+      res.status(201).send(`Criado um novo evento: ${newDoc.id}`);
+  } catch (error) {
+      res.status(400).send(`Deveria conter um name, typeEvent, location, initDate, endDate !!!`);
+  }
+});
+
+// Consulta um evento
+app.get('/eventos/:eventoId', (req,res) => {
+
+  const eventoId = req.params.eventoId; 
+  db.collection(eventoCollection).doc(eventoId).get()
+  .then(evento => {
+      if(!evento.exists) throw new Error('evento not found');
+      res.status(200).json({id:evento.id, data:evento.data()})})
+  .catch(error => res.status(500).send(error));
+      
 });
 
 
+// Deleta um evento 
+app.delete('/eventos/:eventoId', (req, res) => {
+  db.collection(eventoCollection).doc(req.params.eventoId).delete()
+  .then(()=>res.status(204).send("Document successfully deleted!"))
+  .catch(function (error) {
+          res.status(500).send(error);
+  });
+})
 
-export const registrarEvento = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-
-  response.send({name: "Eduardo"});
-});
-
-
-export const obterEvento = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-
-  response.send({name: "Eduardo"});
-});
-
-
-export const listarEventos = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-
-  response.send({name: "Eduardo"});
-});
+// Lista todos os eventos
+app.get('/eventos', async (req, res) => {
+  try {
+    const eventoQuerySnapshot = await db.collection(eventoCollection).get();
+    const eventos: any[] = [];
+    eventoQuerySnapshot.forEach(
+      (doc)=>{
+        eventos.push({
+          id: doc.id,
+          data:doc.data()
+        });
+      }
+    );
+    res.status(200).json(eventos);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  });
---
+++ functions/src/registrarEvento.ts
@@ -0,0 +1,38 @@
+//import * as admin from 'firebase-admin'
+//import { Response } from 'express'
+//
+//const db = admin.firestore()
+//
+//export type Evento = {
+//  id?: string;
+//  description: string;
+//  datahora: EpochTimeStamp;
+//}
+//type Request = {
+//  body: Evento,
+//  params: { eventoId: string}
+//}
+//
+//const registrarEvento = async (req: Request, res: Response) => {
+//  const { description, datahora } = req.body
+//  try {
+//    const evento = db.collection('eventos').doc()
+//    const eventoObject = {
+//      id: evento.id,
+//      description,
+//      datahora,
+//    }
+//
+//    evento.set(eventoObject)
+//
+//    res.status(200).send({
+//      status: 'success',
+//      message: 'entry added successfully',
+//      data: eventoObject
+//    })
+//  } catch(err) {
+//      res.status(500).json(`Internal Server Error: ${err}`)
+//  }
+//}
+//
+//export { registrarEvento }
